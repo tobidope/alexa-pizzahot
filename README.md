0bb9dbca0f82a54664a62430dee961ef52e3b32f
Tobias Bell <tobias.bell@gmail.com>
2017-01-11 00:26:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# alexa-pizzahot
+A playground project to have fun with [Alexa Skills](https://developer.amazon.com/alexa-skills-kit)
+
+## What does it do
+When asking "Alexa, welche Angebote gibt es bei Pizza Hot", it extracts offers from http://www.pizzahot.de/dellbrueck/
+and returns them to Alexa to read them out for you
+
---
+++ app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask_ask import Ask, statement
+import urllib2
+import lxml.etree
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+PIZZA_HOT_URL = 'http://www.pizzahot.de/dellbrueck/'
+
+def get_pizza_hot_offers(url):
+    url_handle = urllib2.urlopen(url)
+    parser = lxml.etree.HTMLParser()
+    tree = lxml.etree.parse(url_handle, parser)
+    offer_elements = tree.findall("//div[@class='r roundedLL']")
+    return [u" ".join(offer[0].text.split()[1:])
+        for offer in offer_elements
+    ]
+
+@ask.intent('GetOffers')
+def get_offers():
+    response = u"Bei Pizza Hot gibt es : {}"\
+        .format(u", ".join(get_pizza_hot_offers(PIZZA_HOT_URL))).encode('utf-8')
+    return statement(response).\
+        simple_card(title="Pizza Hot", content=response)
+
+if __name__ == '__main__':
+    print get_pizza_hot_offers(PIZZA_HOT_URL)
