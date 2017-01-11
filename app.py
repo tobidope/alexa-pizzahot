28452d7e0410520d6661436500527fd470203f36
Tobias Bell <tobias.bell@gmail.com>
2017-01-11 19:11:04 +0100

    Added the interaction model config files and added details to the outpout of offers

--- app.py
+++ app.py
@@ -14,14 +14,15 @@ def get_pizza_hot_offers(url):
     parser = lxml.etree.HTMLParser()
     tree = lxml.etree.parse(url_handle, parser)
     offer_elements = tree.findall("//div[@class='r roundedLL']")
-    return [u" ".join(offer[0].text.split()[1:])
-        for offer in offer_elements
-    ]
+    for offer in offer_elements:
+        title = u''.join(offer[0].text.split()[1:])
+        details = offer[1].text
+        yield title + " " + details
 
 @ask.intent('GetOffers')
 def get_offers():
     response = u"Bei Pizza Hot gibt es : {}"\
-        .format(u", ".join(get_pizza_hot_offers(PIZZA_HOT_URL))).encode('utf-8')
+        .format(u". ".join(get_pizza_hot_offers(PIZZA_HOT_URL))).encode('utf-8')
     return statement(response).\
         simple_card(title="Pizza Hot", content=response)
 
